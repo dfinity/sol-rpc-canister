418ba1d87c5f4a66f20168234d682726b34d4b20
gregorydemay <gregory.demay@dfinity.org>
2025-01-28 22:16:25 +0100

    formatting

--- libs/types/src/tests.rs
+++ libs/types/src/tests.rs
@@ -2,12 +2,16 @@ use crate::DummyRequest;
 
 #[test]
 fn should_deser() {
-    let request = DummyRequest { input: "Hello".to_string() };
+    let request = DummyRequest {
+        input: "Hello".to_string(),
+    };
     let encoded = candid::encode_one(&request).unwrap();
     let decoded: DummyRequest = candid::decode_one(&encoded).unwrap();
     assert_eq!(request, decoded);
 
-    let response = DummyRequest { input: "Hello world!".to_string() };
+    let response = DummyRequest {
+        input: "Hello world!".to_string(),
+    };
     let encoded = candid::encode_one(&response).unwrap();
     let decoded: DummyRequest = candid::decode_one(&encoded).unwrap();
     assert_eq!(response, decoded);
