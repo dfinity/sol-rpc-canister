d653ff3393304a2f68fef9250ea015e83fc2054f
gregorydemay <112856886+gregorydemay@users.noreply.github.com>
2025-01-31 09:56:50 +0100

    build: initial cargo workspace and build pipeline (#2)
    ([XC-280](https://dfinity.atlassian.net/browse/XC-280)): Basic skeleton
    of a Cargo workspace and build pipeline:
    
    1. The Cargo workspace is organized in 4 crates:
    1. The `sol_rpc_canister` crate is a binary crate that will contain the
    canister code.
    2. The `sol_rpc_types` crate is a library that will contain all Candid
    types used by the Candid interface of the canister. This is to
    potentially allow developers to easily write their own client.
    3. The `sol_rpc_client` crate is a library that will contain the client
    code to interact with the canister. This will be the main entry point
    for most developers.
    4. The `sol_rpc_int_tests` is a crate that will contain integration
    tests using PocketIC to test both the canister code directly or the
    client.
    2. The build pipeline has the following stages:
    1. Ensure that the PR title adheres to [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/).
        2. Linting with the Rust formatter and `clippy`
    3. Building the productive wasm by reproducible build. This step is
    currently done only with `cargo` instead of `Docker` and is actually not
    reproducible. This will be addressed in a future PR
    ([XC-283](https://dfinity.atlassian.net/browse/XC-283)).
        4. Running the unit tests.
    5. Running the integration tests with PocketIC using the wasm produced
    by the reproducible stage.
    
    [XC-280]:
    https://dfinity.atlassian.net/browse/XC-280?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [XC-283]:
    https://dfinity.atlassian.net/browse/XC-283?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ integration_tests/src/lib.rs
@@ -0,0 +1,128 @@
+use async_trait::async_trait;
+use candid::utils::ArgumentEncoder;
+use candid::{decode_args, encode_args, CandidType, Encode, Principal};
+use ic_cdk::api::call::RejectionCode;
+use pocket_ic::management_canister::{CanisterId, CanisterSettings};
+use pocket_ic::{nonblocking::PocketIc, PocketIcBuilder, WasmResult};
+use serde::de::DeserializeOwned;
+use sol_rpc_client::{Runtime, SolRpcClient};
+use std::path::PathBuf;
+
+pub struct Setup {
+    env: PocketIc,
+    caller: Principal,
+    _controller: Principal,
+    canister_id: CanisterId,
+}
+
+impl Setup {
+    pub async fn new() -> Self {
+        const DEFAULT_CALLER_TEST_ID: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x01]);
+        const DEFAULT_CONTROLLER_TEST_ID: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x02]);
+
+        let env = PocketIcBuilder::new()
+            .with_fiduciary_subnet()
+            .build_async()
+            .await;
+        let controller = DEFAULT_CONTROLLER_TEST_ID;
+        let canister_id = env
+            .create_canister_with_settings(
+                None,
+                Some(CanisterSettings {
+                    controllers: Some(vec![controller]),
+                    ..CanisterSettings::default()
+                }),
+            )
+            .await;
+        env.add_cycles(canister_id, u128::MAX).await;
+        env.install_canister(
+            canister_id,
+            sol_rpc_wasm(),
+            Encode!().unwrap(),
+            Some(controller),
+        )
+        .await;
+        let caller = DEFAULT_CALLER_TEST_ID;
+
+        Self {
+            env,
+            caller,
+            _controller: controller,
+            canister_id,
+        }
+    }
+
+    pub fn client(&self) -> SolRpcClient<PocketIcRuntime> {
+        SolRpcClient::new(self.new_pocket_ic(), self.canister_id)
+    }
+
+    fn new_pocket_ic(&self) -> PocketIcRuntime {
+        PocketIcRuntime {
+            env: &self.env,
+            caller: self.caller,
+        }
+    }
+
+    pub async fn drop(self) {
+        self.env.drop().await
+    }
+}
+
+fn sol_rpc_wasm() -> Vec<u8> {
+    ic_test_utilities_load_wasm::load_wasm(
+        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("../canister"),
+        "sol_rpc_canister",
+        &[],
+    )
+}
+
+#[derive(Clone)]
+pub struct PocketIcRuntime<'a> {
+    env: &'a PocketIc,
+    caller: Principal,
+}
+
+#[async_trait]
+impl<'a> Runtime for PocketIcRuntime<'a> {
+    async fn call<In, Out>(
+        &self,
+        id: Principal,
+        method: &str,
+        args: In,
+        _cycles: u128,
+    ) -> Result<Out, (RejectionCode, String)>
+    where
+        In: ArgumentEncoder + Send + 'static,
+        Out: CandidType + DeserializeOwned + 'static,
+    {
+        let args_raw = encode_args(args).expect("Failed to encode arguments.");
+        match self
+            .env
+            .update_call(id, self.caller, method, args_raw)
+            .await
+        {
+            Ok(WasmResult::Reply(bytes)) => decode_args(&bytes).map(|(res,)| res).map_err(|e| {
+                (
+                    RejectionCode::CanisterError,
+                    format!(
+                        "failed to decode canister response as {}: {}",
+                        std::any::type_name::<Out>(),
+                        e
+                    ),
+                )
+            }),
+            Ok(WasmResult::Reject(s)) => Err((RejectionCode::CanisterReject, s)),
+            Err(e) => {
+                let rejection_code = match e.code as u64 {
+                    100..=199 => RejectionCode::SysFatal,
+                    200..=299 => RejectionCode::SysTransient,
+                    300..=399 => RejectionCode::DestinationInvalid,
+                    400..=499 => RejectionCode::CanisterReject,
+                    500..=599 => RejectionCode::CanisterError,
+                    _ => RejectionCode::Unknown,
+                };
+                Err((rejection_code, e.description))
+            }
+        }
+    }
+}
