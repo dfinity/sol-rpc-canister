9124277856bd6d8d3819d9d2a4c5963d44011057
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-02-14 17:04:40 +0100

    XC-286: Add method to manage API keys

--- canister/src/main.rs
+++ canister/src/main.rs
@@ -1,9 +1,72 @@
 use candid::candid_method;
-use ic_cdk::query;
+use ic_cdk::api::is_controller;
+use ic_cdk::{query, update};
+use sol_rpc_canister::state::{mutate_state, read_state};
 use sol_rpc_canister::{
     providers::PROVIDERS,
     state::{init_state, State},
 };
+use sol_rpc_canister::providers::find_provider;
+use sol_rpc_types::{ProviderId, RpcAccess};
+
+pub fn require_api_key_principal_or_controller() -> Result<(), String> {
+    let caller = ic_cdk::caller();
+    if read_state(|state| state.is_api_key_principal(&caller)) || is_controller(&caller) {
+        Ok(())
+    } else {
+        Err("You are not authorized".to_string())
+    }
+}
+
+#[query(name = "getProviders")]
+#[candid_method(query, rename = "getProviders")]
+fn get_providers() -> Vec<sol_rpc_types::Provider> {
+    PROVIDERS.with(|providers| providers.clone().into_iter().collect())
+}
+
+#[update(
+    name = "updateApiKeys",
+    guard = "require_api_key_principal_or_controller"
+)]
+#[candid_method(rename = "updateApiKeys")]
+/// Inserts or removes RPC provider API keys.
+///
+/// For each element of `api_keys`, passing `(id, Some(key))` corresponds to inserting or updating
+/// an API key, while passing `(id, None)` indicates that the key should be removed from the canister.
+///
+/// Panics if the list of provider IDs includes a nonexistent or "unauthenticated" (fully public) provider.
+async fn update_api_keys(api_keys: Vec<(ProviderId, Option<String>)>) {
+    // TODO XC-286: Add logs
+    // log!(
+    //     INFO,
+    //     "[{}] Updating API keys for providers: {}",
+    //     ic_cdk::caller(),
+    //     api_keys
+    //         .iter()
+    //         .map(|(id, _)| id.to_string())
+    //         .collect::<Vec<_>>()
+    //         .join(", ")
+    // );
+    for (provider_id, api_key) in api_keys {
+        let provider = find_provider(|provider| provider.provider_id == provider_id)
+            .unwrap_or_else(|| panic!("Provider not found: {}", provider_id));
+        match provider.access {
+            RpcAccess::Authenticated { .. } => {}
+            RpcAccess::Unauthenticated { .. } => {
+                panic!(
+                    "Trying to set API key for unauthenticated provider: {}",
+                    provider_id
+                )
+            }
+        };
+        match api_key {
+            Some(key) => mutate_state(|state| {
+                state.insert_api_key(provider_id, key.try_into().expect("Invalid API key"))
+            }),
+            None => mutate_state(|state| state.remove_api_key(provider_id)),
+        }
+    }
+}
 
 #[ic_cdk::init]
 fn init(args: sol_rpc_types::InstallArgs) {
@@ -15,12 +78,6 @@ fn post_upgrade(args: Option<sol_rpc_types::InstallArgs>) {
     if let Some(args) = args {
         init_state(State::from(args))
     }
-}
-
-#[query(name = "getProviders")]
-#[candid_method(query, rename = "getProviders")]
-fn get_providers() -> Vec<sol_rpc_types::Provider> {
-    PROVIDERS.with(|providers| providers.clone().into_iter().collect())
 }
 
 fn main() {}
