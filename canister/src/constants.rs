091a619fabea496e07ad95550c6a7304623449b0
Paul H. Liu <paul@dfinity.org>
2025-02-27 17:19:09 -0800

    Minor

--- canister/src/constants.rs
+++ canister/src/constants.rs
@@ -4,4 +4,3 @@ pub const VALID_API_KEY_CHARS: &str =
     "0123456789ABCDEFGHIJKLMNOPQRTSUVWXYZabcdefghijklmnopqrstuvwxyz$-_.+!*";
 pub const CONTENT_TYPE_HEADER_LOWERCASE: &str = "content-type";
 pub const CONTENT_TYPE_VALUE: &str = "application/json";
-pub const DEFAULT_NUM_SUBNET_NODES: u32 = 34;
