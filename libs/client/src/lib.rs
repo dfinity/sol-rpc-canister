6f5faef3969279ac4aa394127d4275611ff34b8f
gregorydemay <gregory.demay@dfinity.org>
2025-03-07 17:26:35 +0100

    lint

--- libs/client/src/lib.rs
+++ libs/client/src/lib.rs
@@ -8,8 +8,8 @@ use candid::utils::ArgumentEncoder;
 use candid::{CandidType, Principal};
 use ic_cdk::api::call::RejectionCode;
 use serde::de::DeserializeOwned;
-use solana_clock::Slot;
 use sol_rpc_types::ProviderId;
+use solana_clock::Slot;
 
 /// Abstract the canister runtime so that the client code can be reused:
 /// * in production using `ic_cdk`,
