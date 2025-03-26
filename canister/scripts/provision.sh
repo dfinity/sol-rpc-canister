51e81c96afe1fef3fab49e55e84bb81c3c622b0d
gregorydemay <gregory.demay@dfinity.org>
2025-03-26 18:43:42 +0100

    XC-327: examples script

---
+++ canister/scripts/provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e -x
+
+# Pass API keys by environment variables
+# Fail if the variable is not set
+set -u # or set -o nounset
+: "$ALCHEMY_MAINNET_API_KEY"
+: "$ALCHEMY_DEVNET_API_KEY"
+: "$ANKR_MAINNET_API_KEY"
+: "$ANKR_DEVNET_API_KEY"
+: "$HELIUS_MAINNET_API_KEY"
+: "$HELIUS_DEVNET_API_KEY"
+
+NETWORK="ic"
+IDENTITY="ci"
+WALLET=$(dfx identity get-wallet --network=$NETWORK --identity=$IDENTITY)
+CANISTER="sol_rpc"
+FLAGS="--network=$NETWORK --identity=$IDENTITY --wallet=$WALLET"
+
+dfx canister call ${CANISTER} updateApiKeys "(vec {
+  record { variant { AlchemyMainnet }; opt \"${ALCHEMY_MAINNET_API_KEY}\" };
+  record { variant { AlchemyDevnet }; opt \"${ALCHEMY_DEVNET_API_KEY}\" };
+  record { variant { AnkrMainnet }; opt \"${ANKR_MAINNET_API_KEY}\" };
+  record { variant { AnkrDevnet }; opt \"${ANKR_DEVNET_API_KEY}\" };
+  record { variant { HeliusMainnet }; opt \"${HELIUS_MAINNET_API_KEY}\" };
+  record { variant { HeliusDevnet }; opt \"${HELIUS_DEVNET_API_KEY}\" };
+})" ${FLAGS}
+
