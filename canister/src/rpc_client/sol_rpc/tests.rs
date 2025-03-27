eec5d7b7d10207290aa36b26f99f6b6b700619c6
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-03-27 16:15:16 +0100

    Formatting

--- canister/src/rpc_client/sol_rpc/tests.rs
+++ canister/src/rpc_client/sol_rpc/tests.rs
@@ -3,6 +3,32 @@ use super::*;
 mod normalization_tests {
     use super::*;
 
+    #[test]
+    fn should_normalize_raw_response() {
+        assert_normalized_equal(
+            &ResponseTransform::Raw,
+            r#"{"k1":"v1","k2":"v2"}"#,
+            r#"{"k1":"v1","k2":"v2"}"#,
+        );
+        assert_normalized_equal(
+            &ResponseTransform::Raw,
+            r#"{"k1":"v1","k2":"v2"}"#,
+            r#"{"k2":"v2","k1":"v1"}"#,
+        );
+        assert_normalized_not_equal(
+            &ResponseTransform::Raw,
+            r#"{"k1":"v1","k2":"v2"}"#,
+            r#"{"k1":"v1","k3":"v3"}"#,
+        );
+    }
+
+    #[test]
+    fn should_normalize_get_slot_response() {
+        assert_normalized_equal(&ResponseTransform::GetSlot, "329535108", "329535108");
+        assert_normalized_equal(&ResponseTransform::GetSlot, "329535108", "329535116");
+        assert_normalized_not_equal(&ResponseTransform::GetSlot, "329535108", "329535128");
+    }
+
     fn normalize_response(transform: &ResponseTransform, response: &str) -> String {
         fn add_envelope(reply: &str) -> Vec<u8> {
             format!("{{\"jsonrpc\": \"2.0\", \"id\": 1, \"result\": {}}}", reply).into_bytes()
@@ -24,19 +50,5 @@ mod normalization_tests {
             normalize_response(transform, left),
             normalize_response(transform, right)
         );
-    }
-
-    #[test]
-    fn should_normalize_raw_response() {
-        assert_normalized_equal(&ResponseTransform::Raw, r#"{"k1":"v1","k2":"v2"}"#, r#"{"k1":"v1","k2":"v2"}"#);
-        assert_normalized_equal(&ResponseTransform::Raw, r#"{"k1":"v1","k2":"v2"}"#, r#"{"k2":"v2","k1":"v1"}"#);
-        assert_normalized_not_equal(&ResponseTransform::Raw, r#"{"k1":"v1","k2":"v2"}"#, r#"{"k1":"v1","k3":"v3"}"#);
-    }
-
-    #[test]
-    fn should_normalize_get_slot_response() {
-        assert_normalized_equal(&ResponseTransform::GetSlot, "329535108", "329535108");
-        assert_normalized_equal(&ResponseTransform::GetSlot, "329535108", "329535116");
-        assert_normalized_not_equal(&ResponseTransform::GetSlot, "329535108", "329535128");
     }
 }
