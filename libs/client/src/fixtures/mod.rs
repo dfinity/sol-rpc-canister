71314165d129295ce4ce9c9ec68145642357d174
gregorydemay <gregory.demay@dfinity.org>
2025-04-14 15:16:59 +0200

    XC-331: mocked query and update call responses

--- libs/client/src/fixtures/mod.rs
+++ libs/client/src/fixtures/mod.rs
@@ -11,27 +11,61 @@ use serde::de::DeserializeOwned;
 use sol_rpc_types::{AccountData, AccountEncoding, AccountInfo};
 
 impl<R> ClientBuilder<R> {
-    /// Change the runtime to return a mocked response.
+    /// Change the runtime to return the same mocked response for both update and query calls.
     pub fn with_mocked_response<Out: CandidType>(
         self,
         mocked_response: Out,
     ) -> ClientBuilder<MockRuntime> {
-        self.with_runtime(|_runtime| MockRuntime::new(mocked_response))
+        self.with_runtime(|_runtime| MockRuntime::same_response(mocked_response))
+    }
+
+    /// Change the runtime to return different mocked responses between update and query calls.
+    pub fn with_mocked_responses<UpdateOut: CandidType, QueryOut: CandidType>(
+        self,
+        mocked_response_for_update_call: UpdateOut,
+        mocked_response_for_query_call: QueryOut,
+    ) -> ClientBuilder<MockRuntime> {
+        self.with_runtime(|_runtime| {
+            MockRuntime::new(
+                mocked_response_for_update_call,
+                mocked_response_for_query_call,
+            )
+        })
     }
 }
 
 /// A dummy implementation of [`Runtime`] that always return the same candid-encoded response.
 ///
 /// Implement your own [`Runtime`] in case a more refined approach is needed.
-pub struct MockRuntime(Vec<u8>);
+pub struct MockRuntime {
+    update_call_result: Vec<u8>,
+    query_call_result: Vec<u8>,
+}
 
 impl MockRuntime {
     /// Create a new [`MockRuntime`] to always return the given parameter.
-    pub fn new<Out: CandidType>(mocked_response: Out) -> Self {
-        Self(
-            candid::encode_args((&mocked_response,))
-                .expect("Failed to encode Candid mocked response"),
-        )
+    pub fn same_response<Out: CandidType>(mocked_response: Out) -> Self {
+        let result = candid::encode_args((&mocked_response,))
+            .expect("Failed to encode Candid mocked response");
+        Self {
+            update_call_result: result.clone(),
+            query_call_result: result,
+        }
+    }
+
+    /// Create a new [`MockRuntime`] to always return the given parameters.
+    pub fn new<UpdateOut: CandidType, QueryOut: CandidType>(
+        mocked_update_result: UpdateOut,
+        mocked_query_result: QueryOut,
+    ) -> Self {
+        let update_call_result = candid::encode_args((&mocked_update_result,))
+            .expect("Failed to encode Candid mocked response");
+        let query_call_result = candid::encode_args((&mocked_query_result,))
+            .expect("Failed to encode Candid mocked response");
+        Self {
+            update_call_result,
+            query_call_result,
+        }
     }
 }
 
@@ -48,7 +82,7 @@ impl Runtime for MockRuntime {
         In: ArgumentEncoder + Send,
         Out: CandidType + DeserializeOwned,
     {
-        Ok(candid::decode_args(&self.0)
+        Ok(candid::decode_args(&self.update_call_result)
             .map(|(r,)| r)
             .expect("Failed to decode Candid mocked response"))
     }
@@ -63,7 +97,7 @@ impl Runtime for MockRuntime {
         In: ArgumentEncoder + Send,
         Out: CandidType + DeserializeOwned,
     {
-        Ok(candid::decode_args(&self.0)
+        Ok(candid::decode_args(&self.query_call_result)
             .map(|(r,)| r)
             .expect("Failed to decode Candid mocked response"))
     }
