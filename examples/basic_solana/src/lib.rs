06f0a0f4ba8587a597f2e58f1480f67d0a07e2b2
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-04-15 16:31:19 +0200

    XC-345: Replace manual HTTP outcalls with SOL RPC canister

--- examples/basic_solana/src/lib.rs
+++ examples/basic_solana/src/lib.rs
@@ -1,28 +1,28 @@
 mod ed25519;
-#[allow(deprecated)]
-mod solana_rpc_canister;
 mod solana_wallet;
 mod spl;
 mod state;
+use canhttp::http::json::{ConstantSizeId, Id};
+use solana_nonce::{state::State, versions::Versions as NonceVersions};
 
 use crate::{
-    solana_rpc_canister::{transform_http_request, SolanaRpcCanister},
     solana_wallet::SolanaWallet,
     state::{init_state, read_state},
 };
+use base64::{prelude::BASE64_STANDARD, Engine};
 use candid::{CandidType, Deserialize, Nat, Principal};
-use ic_cdk::{
-    api::management_canister::http_request::{HttpResponse, TransformArgs},
-    init, post_upgrade, query, update,
-};
+use ic_cdk::{init, post_upgrade, update};
 use num::{BigUint, ToPrimitive};
+use serde_json::json;
+use sol_rpc_client::{IcRuntime, SolRpcClient};
+use sol_rpc_types::{GetAccountInfoEncoding, GetAccountInfoParams, RpcSources, SolanaCluster};
+use solana_account_decoder_client_types::{UiAccountData, UiAccountEncoding};
+use solana_hash::Hash;
 use solana_message::Message;
 use solana_program::system_instruction;
 use solana_pubkey::Pubkey;
 use solana_transaction::Transaction;
 use std::{fmt::Display, str::FromStr};
-
-const SOL_RPC: SolanaRpcCanister = SolanaRpcCanister;
 
 #[init]
 pub fn init(init_arg: InitArg) {
@@ -60,63 +60,84 @@ pub async fn associated_token_account(owner: Option<Principal>, mint_account: St
 
 #[update]
 pub async fn get_balance(account: Option<String>) -> Nat {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
-
     let account = account.unwrap_or(solana_account(None).await);
 
-    let json = format!(
-        r#"{{ "jsonrpc": "2.0", "method": "getBalance", "params": ["{}"], "id": 1 }}"#,
-        account
-    );
-
-    let response = SOL_RPC
-        .json_rpc_request(solana_network, json, num_cycles, max_response_size_bytes)
-        .await;
+    let response = client()
+        .json_request(json!({
+            "jsonrpc": "2.0",
+            "id": Id::from(ConstantSizeId::ZERO),
+            "method": "getBalance",
+            "params": [ account ]
+        }))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getBalance` failed");
 
     // The response to a successful `getBalance` call has the following format:
     // { "id": "[ID]", "jsonrpc": "2.0", "result": { "context": { "slot": [SLOT] } }, "value": [BALANCE] }, }
-    let balance = response["result"]["value"].as_u64().unwrap();
+    let balance = serde_json::to_value(response)
+        .expect("`getBalance` response is not a valid JSON")["result"]["value"]
+        .as_u64()
+        .unwrap();
 
     Nat(BigUint::from(balance))
 }
 
 #[update]
 pub async fn get_nonce(account: Option<String>) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
-
     let account = account.unwrap_or(nonce_account(None).await);
 
-    let blockhash = SOL_RPC
-        .get_nonce_account_blockhash(solana_network, num_cycles, max_response_size_bytes, account)
-        .await;
+    // Fetch the account info with the data encoded in base64 format
+    let mut params = GetAccountInfoParams::from_encoded_pubkey(account);
+    params.encoding = Some(GetAccountInfoEncoding::Base64);
+    let account_data = client()
+        .get_account_info(params)
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getAccountInfo` failed")
+        .expect("Account not found for given pubkey")
+        .data;
 
-    blockhash.to_string()
+    // Extract the nonce from the account data
+    let account_data = if let UiAccountData::Binary(blob, UiAccountEncoding::Base64) = account_data
+    {
+        BASE64_STANDARD
+            .decode(blob)
+            .expect("Unable to base64 decode account data")
+    } else {
+        panic!("Invalid response format");
+    };
+    match bincode::deserialize::<NonceVersions>(account_data.as_slice())
+        .expect("Failed to deserialize nonce account data")
+        .state()
+    {
+        State::Uninitialized => panic!("Nonce account is uninitialized"),
+        State::Initialized(data) => data.blockhash().to_string(),
+    }
 }
 
 #[update]
 pub async fn get_spl_token_balance(account: Option<String>, mint_account: String) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
-
     let account = account.unwrap_or(associated_token_account(None, mint_account).await);
 
-    let json = format!(
-        r#"{{ "jsonrpc": "2.0", "method": "getTokenAccountBalance", "params": ["{}"], "id": 1 }}"#,
-        account
-    );
-
-    let response = SOL_RPC
-        .json_rpc_request(solana_network, json, num_cycles, max_response_size_bytes)
-        .await;
+    let response = client()
+        .json_request(json!({
+            "jsonrpc": "2.0",
+            "id": Id::from(ConstantSizeId::ZERO),
+            "method": "getTokenAccountBalance",
+            "params": [ account ]
+        }))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getTokenAccountBalance` failed");
 
     // The response to a successful `getTokenAccountBalance` call has the following format:
     // { "id": "[ID]", "jsonrpc": "2.0", "result": { "context": { "slot": [SLOT] } }, "value": [ { "uiAmountString": "FORMATTED AMOUNT" } ] }, }
-    response["result"]["value"]["uiAmountString"]
+    serde_json::to_value(response).expect("`getTokenAccountBalance` response is not a valid JSON")
+        ["result"]["value"]["uiAmountString"]
         .as_str()
         .unwrap()
         .to_string()
@@ -124,9 +145,7 @@ pub async fn get_spl_token_balance(account: Option<String>, mint_account: String
 
 #[update]
 pub async fn create_nonce_account(owner: Option<Principal>) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
+    let client = client();
 
     let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
     let wallet = SolanaWallet::new(owner).await;
@@ -140,12 +159,12 @@ pub async fn create_nonce_account(owner: Option<Principal>) -> String {
         payer.as_ref(),
         1_500_000,
     );
-    let blockhash = SOL_RPC
-        .get_latest_blockhash(solana_network, num_cycles, max_response_size_bytes)
-        .await;
 
-    let instruction = instructions.as_slice();
-    let message = Message::new_with_blockhash(instruction, Some(payer.as_ref()), &blockhash);
+    let message = Message::new_with_blockhash(
+        instructions.as_slice(),
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
 
     let signatures = vec![
         wallet.sign_with_ed25519(&message, &payer).await,
@@ -156,14 +175,13 @@ pub async fn create_nonce_account(owner: Option<Principal>) -> String {
         signatures,
     };
 
-    SOL_RPC
-        .send_transaction(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            transaction,
-        )
+    client
+        .send_transaction(transaction)
+        .send()
         .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
 }
 
 #[update]
@@ -171,9 +189,7 @@ pub async fn create_associated_token_account(
     owner: Option<Principal>,
     mint_account: String,
 ) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
+    let client = client();
 
     let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
     let wallet = SolanaWallet::new(owner).await;
@@ -183,11 +199,12 @@ pub async fn create_associated_token_account(
 
     let instruction =
         spl::create_associated_token_account_instruction(payer.as_ref(), payer.as_ref(), &mint);
-    let blockhash = SOL_RPC
-        .get_latest_blockhash(solana_network, num_cycles, max_response_size_bytes)
-        .await;
 
-    let message = Message::new_with_blockhash(&[instruction], Some(payer.as_ref()), &blockhash);
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
 
     let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
     let transaction = Transaction {
@@ -195,21 +212,18 @@ pub async fn create_associated_token_account(
         signatures,
     };
 
-    SOL_RPC
-        .send_transaction(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            transaction,
-        )
+    client
+        .send_transaction(transaction)
+        .send()
         .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
 }
 
 #[update]
 pub async fn send_sol(owner: Option<Principal>, to: String, amount: Nat) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
+    let client = client();
 
     let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
     let wallet = SolanaWallet::new(owner).await;
@@ -219,25 +233,25 @@ pub async fn send_sol(owner: Option<Principal>, to: String, amount: Nat) -> Stri
     let amount = amount.0.to_u64().unwrap();
 
     let instruction = system_instruction::transfer(payer.as_ref(), &recipient, amount);
-    let blockhash = SOL_RPC
-        .get_latest_blockhash(solana_network, num_cycles, max_response_size_bytes)
-        .await;
 
-    let message = Message::new_with_blockhash(&[instruction], Some(payer.as_ref()), &blockhash);
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
     let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
     let transaction = Transaction {
         message,
         signatures,
     };
 
-    SOL_RPC
-        .send_transaction(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            transaction,
-        )
+    client
+        .send_transaction(transaction)
+        .send()
         .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
 }
 
 #[update]
@@ -246,9 +260,7 @@ pub async fn send_sol_with_durable_nonce(
     to: String,
     amount: Nat,
 ) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
+    let client = client();
 
     let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
     let wallet = SolanaWallet::new(owner).await;
@@ -262,14 +274,9 @@ pub async fn send_sol_with_durable_nonce(
         system_instruction::advance_nonce_account(nonce_account.as_ref(), payer.as_ref()),
         system_instruction::transfer(payer.as_ref(), &recipient, amount),
     ];
-    let blockhash = SOL_RPC
-        .get_nonce_account_blockhash(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            nonce_account.to_string(),
-        )
-        .await;
+
+    let blockhash = Hash::from_str(&get_nonce(Some(nonce_account.to_string())).await)
+        .expect("Unable to parse nonce as blockhash");
 
     let message = Message::new_with_blockhash(instructions, Some(payer.as_ref()), &blockhash);
     let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
@@ -278,14 +285,13 @@ pub async fn send_sol_with_durable_nonce(
         signatures,
     };
 
-    SOL_RPC
-        .send_transaction(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            transaction,
-        )
+    client
+        .send_transaction(transaction)
+        .send()
         .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
 }
 
 #[update]
@@ -295,9 +301,7 @@ pub async fn send_spl_token(
     to: String,
     amount: Nat,
 ) -> String {
-    let solana_network = read_state(|s| s.solana_network());
-    let max_response_size_bytes = 500_u64;
-    let num_cycles = 1_000_000_000u128;
+    let client = client();
 
     let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
     let wallet = SolanaWallet::new(owner).await;
@@ -311,31 +315,51 @@ pub async fn send_spl_token(
     let to = spl::get_associated_token_address(&recipient, &mint);
 
     let instruction = spl::transfer_instruction(&from, &to, payer.as_ref(), amount);
-    let blockhash = SOL_RPC
-        .get_latest_blockhash(solana_network, num_cycles, max_response_size_bytes)
-        .await;
 
-    let message = Message::new_with_blockhash(&[instruction], Some(payer.as_ref()), &blockhash);
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
     let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
     let transaction = Transaction {
         message,
         signatures,
     };
 
-    SOL_RPC
-        .send_transaction(
-            solana_network,
-            num_cycles,
-            max_response_size_bytes,
-            transaction,
-        )
+    client
+        .send_transaction(transaction)
+        .send()
         .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
 }
 
-// TODO: Remove!
-#[query(name = "__transform_json_rpc", hidden = true)]
-fn transform(args: TransformArgs) -> HttpResponse {
-    transform_http_request(args)
+async fn get_recent_blockhash(rpc_client: &SolRpcClient<IcRuntime>) -> Hash {
+    let slot = rpc_client
+        .get_slot()
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getSlot` failed");
+    let blockhash = rpc_client
+        .get_block(slot)
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getBlock` failed")
+        .unwrap_or_else(|| panic!("Block for slot {slot} not found"))
+        .blockhash;
+    Hash::from_str(&blockhash).expect("Unable to parse blockhash")
+}
+
+fn client() -> SolRpcClient<IcRuntime> {
+    SolRpcClient::builder_for_ic()
+        .with_rpc_sources(RpcSources::Default(
+            read_state(|state| state.solana_network()).into(),
+        ))
+        .build()
 }
 
 #[derive(CandidType, Deserialize, Debug, Default, PartialEq, Eq)]
@@ -350,6 +374,16 @@ pub enum SolanaNetwork {
     #[default]
     Devnet,
     Testnet,
+}
+
+impl From<SolanaNetwork> for SolanaCluster {
+    fn from(network: SolanaNetwork) -> Self {
+        match network {
+            SolanaNetwork::Mainnet => Self::Mainnet,
+            SolanaNetwork::Devnet => Self::Devnet,
+            SolanaNetwork::Testnet => Self::Testnet,
+        }
+    }
 }
 
 #[derive(CandidType, Deserialize, Debug, Default, PartialEq, Eq, Clone)]
