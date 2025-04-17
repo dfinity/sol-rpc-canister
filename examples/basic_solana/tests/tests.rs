1938e9d26ffb663c5ac99e3ddce982b538904938
gregorydemay <gregory.demay@dfinity.org>
2025-04-17 17:26:46 +0200

    XC-349: install SOL RPC and basic solana canisters

---
+++ examples/basic_solana/tests/tests.rs
@@ -0,0 +1,93 @@
+use basic_solana::{Ed25519KeyName, SolanaNetwork};
+use candid::{Encode, Principal};
+use pocket_ic::management_canister::{CanisterId, CanisterSettings};
+use pocket_ic::{PocketIc, PocketIcBuilder};
+use sol_rpc_types::{OverrideProvider, RegexSubstitution};
+use std::env::var;
+use std::path::PathBuf;
+
+#[test]
+fn test_basic_solana() {
+    let setup = Setup::new();
+}
+
+pub struct Setup {
+    env: PocketIc,
+    sol_rpc_canister_id: CanisterId,
+    basic_solana_canister_id: CanisterId,
+}
+
+impl Setup {
+    pub const DEFAULT_CONTROLLER: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x02]);
+    const SOLANA_VALIDATOR_URL: &'static str = "http://localhost:8899";
+
+    pub fn new() -> Self {
+        let env = PocketIcBuilder::new().with_fiduciary_subnet().build();
+
+        let sol_rpc_canister_id = env.create_canister_with_settings(
+            None,
+            Some(CanisterSettings {
+                controllers: Some(vec![Self::DEFAULT_CONTROLLER]),
+                ..CanisterSettings::default()
+            }),
+        );
+        env.add_cycles(sol_rpc_canister_id, u64::MAX as u128);
+        let sol_rpc_install_args = sol_rpc_types::InstallArgs {
+            override_provider: Some(OverrideProvider {
+                override_url: Some(RegexSubstitution {
+                    pattern: ".*".into(),
+                    replacement: Self::SOLANA_VALIDATOR_URL.to_string(),
+                }),
+            }),
+            ..Default::default()
+        };
+        env.install_canister(
+            sol_rpc_canister_id,
+            sol_rpc_wasm(),
+            Encode!(&sol_rpc_install_args).unwrap(),
+            Some(Self::DEFAULT_CONTROLLER),
+        );
+
+        let basic_solana_canister_id = env.create_canister();
+        env.add_cycles(basic_solana_canister_id, u64::MAX as u128);
+        let basic_solana_install_args = basic_solana::InitArg {
+            sol_rpc_canister_id: Some(basic_solana_canister_id),
+            solana_network: Some(SolanaNetwork::Mainnet),
+            ed25519_key_name: Some(Ed25519KeyName::ProductionKey1),
+        };
+        env.install_canister(
+            basic_solana_canister_id,
+            basic_solana_wasm(),
+            Encode!(&basic_solana_install_args).unwrap(),
+            None,
+        );
+
+        Self {
+            env,
+            sol_rpc_canister_id,
+            basic_solana_canister_id,
+        }
+    }
+}
+
+impl Default for Setup {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn sol_rpc_wasm() -> Vec<u8> {
+    ic_test_utilities_load_wasm::load_wasm(
+        PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).join("../../canister"),
+        "sol_rpc_canister",
+        &[],
+    )
+}
+
+fn basic_solana_wasm() -> Vec<u8> {
+    ic_test_utilities_load_wasm::load_wasm(
+        PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).join("."),
+        "basic_solana",
+        &[],
+    )
+}
