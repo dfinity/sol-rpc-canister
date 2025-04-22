db50c8e9844d5c5197eec8a811d9203d93440619
gregorydemay <gregory.demay@dfinity.org>
2025-04-22 21:36:43 +0200

    XC-349: add check_candid_interface_compatibility check

--- examples/basic_solana/src/main.rs
+++ examples/basic_solana/src/main.rs
@@ -352,4 +352,24 @@ pub async fn send_spl_token(
         .expect("Call to `sendTransaction` failed")
         .to_string()
 }
+
 fn main() {}
+
+#[test]
+fn check_candid_interface_compatibility() {
+    use candid_parser::utils::{service_equal, CandidSource};
+
+    candid::export_service!();
+
+    let new_interface = __export_service();
+
+    // check the public interface against the actual one
+    let old_interface = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("basic_solana.did");
+
+    service_equal(
+        CandidSource::Text(dbg!(&new_interface)),
+        CandidSource::File(old_interface.as_path()),
+    )
+    .unwrap();
+}
