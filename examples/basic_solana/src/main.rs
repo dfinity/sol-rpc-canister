e6ea719198d41b25454d1b610413741b9f5416cf
gregorydemay <112856886+gregorydemay@users.noreply.github.com>
2025-04-22 21:54:12 +0200

    test: integration tests for the `basic_solana` example (#75)
    Ensure that the instructions for trying out the `basic_solana` example
    are up-to-date by adding the following integration test:
    
    1. Local setup with Pocket IC and an instance of the SOL RPC canister
    that uses a local Solana test validator for RPC providers.
    2. Refactor `basic_solana` to split the code between a `main.rs` and a
    `lib.rs`, so that public types can be re-used in integration tests.
    3. The test follows the instructions in the
    `examples/basic_solana/README.md` file.
    
    Left for future PRs:
    
    1. Testing SPL tokens is currently problematic because the dependency
    `https://crates.io/crates/spl-token-2022` needed to craft instructions
    related to SPL tokens brings many conflicts among its transitive
    dependencies. This could be avoided by using the `spl-token-cli` command
    line tool.
    2. The Solana RPC client that interacts with the SOL RPC canister should
    offer a global commitment level. That will be addressed in a separate
    ticket ([XC-350](https://dfinity.atlassian.net/browse/XC-350))
    
    [XC-350]:
    https://dfinity.atlassian.net/browse/XC-350?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ examples/basic_solana/src/main.rs
@@ -0,0 +1,375 @@
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
+use basic_solana::solana_wallet::SolanaWallet;
+use basic_solana::state::init_state;
+use basic_solana::{client, get_recent_blockhash, spl, validate_caller_not_anonymous, InitArg};
+use candid::{Nat, Principal};
+use ic_cdk::{init, post_upgrade, update};
+use num::ToPrimitive;
+use serde_json::json;
+use sol_rpc_types::{CommitmentLevel, GetAccountInfoEncoding, GetAccountInfoParams};
+use solana_account_decoder_client_types::{UiAccountData, UiAccountEncoding};
+use solana_hash::Hash;
+use solana_message::Message;
+use solana_nonce::{state::State, versions::Versions as NonceVersions};
+use solana_program::system_instruction;
+use solana_pubkey::Pubkey;
+use solana_transaction::Transaction;
+use std::str::FromStr;
+
+#[init]
+pub fn init(init_arg: InitArg) {
+    init_state(init_arg)
+}
+
+#[post_upgrade]
+fn post_upgrade(init_arg: Option<InitArg>) {
+    if let Some(init_arg) = init_arg {
+        init_state(init_arg)
+    }
+}
+
+#[update]
+pub async fn solana_account(owner: Option<Principal>) -> String {
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+    wallet.solana_account().to_string()
+}
+
+#[update]
+pub async fn nonce_account(owner: Option<Principal>) -> String {
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+    wallet.derived_nonce_account().to_string()
+}
+
+#[update]
+pub async fn associated_token_account(owner: Option<Principal>, mint_account: String) -> String {
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let mint = Pubkey::from_str(&mint_account).unwrap();
+    let wallet = SolanaWallet::new(owner).await;
+    spl::get_associated_token_address(wallet.solana_account().as_ref(), &mint).to_string()
+}
+
+#[update]
+pub async fn get_balance(account: Option<String>) -> Nat {
+    let account = account.unwrap_or(solana_account(None).await);
+    let public_key = Pubkey::from_str(&account).unwrap();
+    let balance = client()
+        .get_balance(public_key)
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getBalance` failed");
+
+    Nat::from(balance)
+}
+
+#[update]
+pub async fn get_nonce(account: Option<String>) -> String {
+    let account = account.unwrap_or(nonce_account(None).await);
+
+    // Fetch the account info with the data encoded in base64 format
+    // TODO XC-347: use method from client to retrieve nonce
+    let mut params = GetAccountInfoParams::from_encoded_pubkey(account);
+    params.encoding = Some(GetAccountInfoEncoding::Base64);
+    let account_data = client()
+        .get_account_info(params)
+        // TODO XC-350: use commitment level from client
+        .modify_params(|params| params.commitment = Some(CommitmentLevel::Confirmed))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getAccountInfo` failed")
+        .expect("Account not found for given pubkey")
+        .data;
+
+    // Extract the nonce from the account data
+    let account_data = if let UiAccountData::Binary(blob, UiAccountEncoding::Base64) = account_data
+    {
+        BASE64_STANDARD
+            .decode(blob)
+            .expect("Unable to base64 decode account data")
+    } else {
+        panic!("Invalid response format");
+    };
+    match bincode::deserialize::<NonceVersions>(account_data.as_slice())
+        .expect("Failed to deserialize nonce account data")
+        .state()
+    {
+        State::Uninitialized => panic!("Nonce account is uninitialized"),
+        State::Initialized(data) => data.blockhash().to_string(),
+    }
+}
+
+#[update]
+pub async fn get_spl_token_balance(account: Option<String>, mint_account: String) -> String {
+    let account = account.unwrap_or(associated_token_account(None, mint_account).await);
+
+    // TODO XC-325: use `getTokenAccountBalance` method from client
+    let response = client()
+        .json_request(json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getTokenAccountBalance",
+            "params": [ account ]
+        }))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `getTokenAccountBalance` failed");
+
+    // The response to a successful `getTokenAccountBalance` call has the following format:
+    // { "id": "[ID]", "jsonrpc": "2.0", "result": { "context": { "slot": [SLOT] } }, "value": [ { "uiAmountString": "FORMATTED AMOUNT" } ] }, }
+    serde_json::to_value(response).expect("`getTokenAccountBalance` response is not a valid JSON")
+        ["result"]["value"]["uiAmountString"]
+        .as_str()
+        .unwrap()
+        .to_string()
+}
+
+#[update]
+pub async fn create_nonce_account(owner: Option<Principal>) -> String {
+    let client = client();
+
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+
+    let payer = wallet.solana_account();
+    let nonce_account = wallet.derived_nonce_account();
+
+    if let Some(_account) = client
+        .get_account_info(*nonce_account.as_ref())
+        // TODO XC-350: use commitment level from client
+        .modify_params(|params| params.commitment = Some(CommitmentLevel::Confirmed))
+        .send()
+        .await
+        .expect_consistent()
+        .unwrap_or_else(|e| {
+            panic!(
+                "Call to `getAccountInfo` for {} failed: {e}",
+                nonce_account.as_ref()
+            )
+        })
+    {
+        ic_cdk::println!(
+            "[create_nonce_account]: Account {} already exists. Skipping creation of nonce account",
+            nonce_account.as_ref()
+        );
+        return nonce_account.as_ref().to_string();
+    }
+
+    let instructions = system_instruction::create_nonce_account(
+        payer.as_ref(),
+        nonce_account.as_ref(),
+        payer.as_ref(),
+        1_500_000,
+    );
+
+    let message = Message::new_with_blockhash(
+        instructions.as_slice(),
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
+
+    let signatures = vec![
+        wallet.sign_with_ed25519(&message, &payer).await,
+        wallet.sign_with_ed25519(&message, &nonce_account).await,
+    ];
+    let transaction = Transaction {
+        message,
+        signatures,
+    };
+
+    client
+        .send_transaction(transaction)
+        // TODO XC-350: use commitment level from client
+        .modify_params(|params| params.preflight_commitment = Some(CommitmentLevel::Confirmed))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed");
+
+    nonce_account.as_ref().to_string()
+}
+
+#[update]
+pub async fn create_associated_token_account(
+    owner: Option<Principal>,
+    mint_account: String,
+) -> String {
+    let client = client();
+
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+
+    let payer = wallet.solana_account();
+    let mint = Pubkey::from_str(&mint_account).unwrap();
+
+    let instruction =
+        spl::create_associated_token_account_instruction(payer.as_ref(), payer.as_ref(), &mint);
+
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
+
+    let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
+    let transaction = Transaction {
+        message,
+        signatures,
+    };
+
+    client
+        .send_transaction(transaction)
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
+}
+
+#[update]
+pub async fn send_sol(owner: Option<Principal>, to: String, amount: Nat) -> String {
+    let client = client();
+
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+
+    let recipient = Pubkey::from_str(&to).unwrap();
+    let payer = wallet.solana_account();
+    let amount = amount.0.to_u64().unwrap();
+
+    ic_cdk::println!(
+        "Instruction to transfer {amount} lamports from {} to {recipient}",
+        payer.as_ref()
+    );
+    let instruction = system_instruction::transfer(payer.as_ref(), &recipient, amount);
+
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
+    let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
+    let transaction = Transaction {
+        message,
+        signatures,
+    };
+
+    client
+        .send_transaction(transaction)
+        // TODO XC-350: use commitment level from client
+        .modify_params(|params| params.preflight_commitment = Some(CommitmentLevel::Confirmed))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
+}
+
+#[update]
+pub async fn send_sol_with_durable_nonce(
+    owner: Option<Principal>,
+    to: String,
+    amount: Nat,
+) -> String {
+    let client = client();
+
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+
+    let recipient = Pubkey::from_str(&to).unwrap();
+    let payer = wallet.solana_account();
+    let amount = amount.0.to_u64().unwrap();
+    let nonce_account = wallet.derived_nonce_account();
+
+    let instructions = &[
+        system_instruction::advance_nonce_account(nonce_account.as_ref(), payer.as_ref()),
+        system_instruction::transfer(payer.as_ref(), &recipient, amount),
+    ];
+
+    let blockhash = Hash::from_str(&get_nonce(Some(nonce_account.to_string())).await)
+        .expect("Unable to parse nonce as blockhash");
+
+    let message = Message::new_with_blockhash(instructions, Some(payer.as_ref()), &blockhash);
+    let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
+    let transaction = Transaction {
+        message,
+        signatures,
+    };
+
+    client
+        .send_transaction(transaction)
+        // TODO XC-350: use commitment level from client
+        .modify_params(|params| params.preflight_commitment = Some(CommitmentLevel::Confirmed))
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
+}
+
+#[update]
+pub async fn send_spl_token(
+    owner: Option<Principal>,
+    mint_account: String,
+    to: String,
+    amount: Nat,
+) -> String {
+    let client = client();
+
+    let owner = owner.unwrap_or_else(validate_caller_not_anonymous);
+    let wallet = SolanaWallet::new(owner).await;
+
+    let payer = wallet.solana_account();
+    let recipient = Pubkey::from_str(&to).unwrap();
+    let mint = Pubkey::from_str(&mint_account).unwrap();
+    let amount = amount.0.to_u64().unwrap();
+
+    let from = spl::get_associated_token_address(payer.as_ref(), &mint);
+    let to = spl::get_associated_token_address(&recipient, &mint);
+
+    let instruction = spl::transfer_instruction(&from, &to, payer.as_ref(), amount);
+
+    let message = Message::new_with_blockhash(
+        &[instruction],
+        Some(payer.as_ref()),
+        &get_recent_blockhash(&client).await,
+    );
+    let signatures = vec![wallet.sign_with_ed25519(&message, &payer).await];
+    let transaction = Transaction {
+        message,
+        signatures,
+    };
+
+    client
+        .send_transaction(transaction)
+        .send()
+        .await
+        .expect_consistent()
+        .expect("Call to `sendTransaction` failed")
+        .to_string()
+}
+
+fn main() {}
+
+#[test]
+fn check_candid_interface_compatibility() {
+    use candid_parser::utils::{service_equal, CandidSource};
+
+    candid::export_service!();
+
+    let new_interface = __export_service();
+
+    // check the public interface against the actual one
+    let old_interface = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("basic_solana.did");
+
+    service_equal(
+        CandidSource::Text(dbg!(&new_interface)),
+        CandidSource::File(old_interface.as_path()),
+    )
+    .unwrap();
+}
---
+++ examples/basic_solana/tests/tests.rs
@@ -0,0 +1,312 @@
+use basic_solana::{Ed25519KeyName, SolanaNetwork};
+use candid::utils::ArgumentEncoder;
+use candid::{decode_args, encode_args, CandidType, Encode, Nat, Principal};
+use pocket_ic::management_canister::{CanisterId, CanisterSettings};
+use pocket_ic::{PocketIc, PocketIcBuilder};
+use serde::de::DeserializeOwned;
+use sol_rpc_types::{
+    OverrideProvider, RegexSubstitution, RpcAccess, SupportedRpcProvider, SupportedRpcProviderId,
+};
+use solana_client::rpc_client::RpcClient as SolanaRpcClient;
+use solana_commitment_config::CommitmentConfig;
+use solana_hash::Hash;
+use solana_pubkey::{pubkey, Pubkey};
+use solana_signature::Signature;
+use std::env::var;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+pub const USER: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x42]);
+pub const AIRDROP_AMOUNT: u64 = 1_000_000_000; // 1 SOL
+
+// *NOTE*: Update instructions in README.md if you change this test!
+#[test]
+fn test_basic_solana() {
+    let setup = Setup::new().with_mock_api_keys();
+    let basic_solana = setup.basic_solana();
+
+    // ## Step 2: Generating a Solana account
+    let user_solana_account: Pubkey = basic_solana
+        .update_call::<_, String>(USER, "solana_account", ())
+        .parse()
+        .expect("Failed to parse public key");
+
+    // ## Step 3: Receiving SOL
+    setup.airdrop(&user_solana_account, AIRDROP_AMOUNT);
+    println!("User solana account {user_solana_account}");
+
+    let receiver_solana_account = pubkey!("8HNiduWaBanrBv8c2pgGXZWnpKBdEYuQNHnspqto4yyq");
+    assert_ne!(user_solana_account, receiver_solana_account);
+    // The receiver account must be Initialized before receiving SOL,
+    // which will be done when requesting an airdrop
+    setup.airdrop(&receiver_solana_account, AIRDROP_AMOUNT);
+    println!("Receiver solana account {receiver_solana_account}");
+
+    // ## Step 4: Sending SOL
+    let receiver_balance_before = setup
+        .solana_client
+        .get_balance(&receiver_solana_account)
+        .unwrap();
+    let send_sol_tx: Signature = basic_solana
+        .update_call::<_, String>(
+            USER,
+            "send_sol",
+            (
+                None::<Principal>,
+                receiver_solana_account.to_string(),
+                Nat::from(1_u8),
+            ),
+        )
+        .parse()
+        .unwrap();
+    let expected_receiver_balance = receiver_balance_before + 1;
+    assert_eq!(
+        setup.solana_client.wait_for_balance_with_commitment(
+            &receiver_solana_account,
+            Some(expected_receiver_balance),
+            CommitmentConfig::confirmed()
+        ),
+        Some(expected_receiver_balance)
+    );
+    assert!(setup
+        .solana_client
+        .confirm_transaction(&send_sol_tx)
+        .unwrap());
+
+    // ## Step 5: Sending SOL using durable nonces
+    let nonce_account: Pubkey = basic_solana
+        .update_call::<_, String>(USER, "create_nonce_account", ())
+        .parse()
+        .unwrap();
+    setup.solana_client.wait_for_balance_with_commitment(
+        &nonce_account,
+        Some(1_500_000),
+        CommitmentConfig::confirmed(),
+    );
+    let nonce_1 = setup.ensure_nonce_consistent(&nonce_account);
+
+    let receiver_balance_before = setup
+        .solana_client
+        .get_balance(&receiver_solana_account)
+        .unwrap();
+    let send_sol_tx: Signature = basic_solana
+        .update_call::<_, String>(
+            USER,
+            "send_sol_with_durable_nonce",
+            (
+                None::<Principal>,
+                receiver_solana_account.to_string(),
+                Nat::from(1_u8),
+            ),
+        )
+        .parse()
+        .unwrap();
+    let expected_receiver_balance = receiver_balance_before + 1;
+    assert_eq!(
+        setup.solana_client.wait_for_balance_with_commitment(
+            &receiver_solana_account,
+            Some(expected_receiver_balance),
+            CommitmentConfig::confirmed()
+        ),
+        Some(expected_receiver_balance)
+    );
+    assert!(setup
+        .solana_client
+        .confirm_transaction(&send_sol_tx)
+        .unwrap());
+    let nonce_2 = setup.ensure_nonce_consistent(&nonce_account);
+    assert_ne!(nonce_1, nonce_2);
+
+    // ## Step 6: Sending Solana Program Library (SPL) tokens
+    // TODO: XC-349 test SPL tokens, adding the spl_token_2022 dependency brings a lot of problems
+    // with conflicting versions of transitive dependencies.
+}
+
+pub struct Setup {
+    env: Arc<PocketIc>,
+    solana_client: SolanaRpcClient,
+    sol_rpc_canister_id: CanisterId,
+    basic_solana_canister_id: CanisterId,
+}
+
+impl Setup {
+    pub const DEFAULT_CONTROLLER: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x02]);
+    const SOLANA_VALIDATOR_URL: &'static str = "http://localhost:8899";
+
+    pub fn new() -> Self {
+        let env = PocketIcBuilder::new()
+            .with_nns_subnet() //make_live requires NNS subnet.
+            .with_fiduciary_subnet()
+            .build();
+
+        let sol_rpc_canister_id = env.create_canister_with_settings(
+            None,
+            Some(CanisterSettings {
+                controllers: Some(vec![Self::DEFAULT_CONTROLLER]),
+                ..CanisterSettings::default()
+            }),
+        );
+        env.add_cycles(sol_rpc_canister_id, u64::MAX as u128);
+        let sol_rpc_install_args = sol_rpc_types::InstallArgs {
+            override_provider: Some(OverrideProvider {
+                override_url: Some(RegexSubstitution {
+                    pattern: ".*".into(),
+                    replacement: Self::SOLANA_VALIDATOR_URL.to_string(),
+                }),
+            }),
+            ..Default::default()
+        };
+        env.install_canister(
+            sol_rpc_canister_id,
+            sol_rpc_wasm(),
+            Encode!(&sol_rpc_install_args).unwrap(),
+            Some(Self::DEFAULT_CONTROLLER),
+        );
+
+        let basic_solana_canister_id = env.create_canister();
+        env.add_cycles(basic_solana_canister_id, u64::MAX as u128);
+        let basic_solana_install_args = basic_solana::InitArg {
+            sol_rpc_canister_id: Some(sol_rpc_canister_id),
+            solana_network: Some(SolanaNetwork::Devnet),
+            ed25519_key_name: Some(Ed25519KeyName::ProductionKey1),
+        };
+        env.install_canister(
+            basic_solana_canister_id,
+            basic_solana_wasm(),
+            Encode!(&basic_solana_install_args).unwrap(),
+            None,
+        );
+
+        println!("Basic solana canister ID {basic_solana_canister_id}");
+        println!("SOL RPC canister id {sol_rpc_canister_id}");
+        let mut env = env;
+        let _endpoint = env.make_live(None);
+        Self {
+            env: Arc::new(env),
+            solana_client: SolanaRpcClient::new_with_commitment(
+                Self::SOLANA_VALIDATOR_URL,
+                // Using confirmed commitment in tests provides faster execution while maintaining
+                // sufficient reliability.
+                CommitmentConfig::confirmed(),
+            ),
+            sol_rpc_canister_id,
+            basic_solana_canister_id,
+        }
+    }
+
+    fn with_mock_api_keys(self) -> Self {
+        const MOCK_API_KEY: &str = "mock-api-key";
+        let sol_rpc = self.sol_rpc();
+        let providers: Vec<(SupportedRpcProviderId, SupportedRpcProvider)> =
+            sol_rpc.update_call(Principal::anonymous(), "getProviders", ());
+        let mut api_keys = Vec::new();
+        for (id, provider) in providers {
+            match provider.access {
+                RpcAccess::Authenticated { .. } => {
+                    api_keys.push((id, Some(MOCK_API_KEY.to_string())));
+                }
+                RpcAccess::Unauthenticated { .. } => {}
+            }
+        }
+        let _res: () = sol_rpc.update_call(Self::DEFAULT_CONTROLLER, "updateApiKeys", (api_keys,));
+        self
+    }
+
+    fn airdrop(&self, account: &Pubkey, amount: u64) {
+        let balance_before = self.solana_client.get_balance(account).unwrap();
+        let _airdrop_tx = self.solana_client.request_airdrop(account, amount).unwrap();
+        let expected_balance = balance_before + amount;
+        assert_eq!(
+            self.solana_client.wait_for_balance_with_commitment(
+                account,
+                Some(expected_balance),
+                CommitmentConfig::confirmed()
+            ),
+            Some(expected_balance)
+        );
+    }
+
+    fn ensure_nonce_consistent(&self, nonce_account: &Pubkey) -> Hash {
+        let expected_nonce: Hash = self
+            .basic_solana()
+            .update_call::<_, String>(USER, "get_nonce", (Some(nonce_account.to_string()),))
+            .parse()
+            .unwrap();
+        let actual_nonce = solana_rpc_client_nonce_utils::data_from_account(
+            &self.solana_client.get_account(nonce_account).unwrap(),
+        )
+        .unwrap()
+        .blockhash();
+        assert_eq!(expected_nonce, actual_nonce);
+        expected_nonce
+    }
+
+    fn sol_rpc(&self) -> Canister {
+        Canister {
+            env: self.env.clone(),
+            id: self.sol_rpc_canister_id,
+        }
+    }
+
+    fn basic_solana(&self) -> Canister {
+        Canister {
+            env: self.env.clone(),
+            id: self.basic_solana_canister_id,
+        }
+    }
+}
+
+impl Default for Setup {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn sol_rpc_wasm() -> Vec<u8> {
+    ic_test_utilities_load_wasm::load_wasm(
+        PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).join("../../canister"),
+        "sol_rpc_canister",
+        &[],
+    )
+}
+
+fn basic_solana_wasm() -> Vec<u8> {
+    ic_test_utilities_load_wasm::load_wasm(
+        PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).join("."),
+        "basic_solana",
+        &[],
+    )
+}
+
+pub struct Canister {
+    env: Arc<PocketIc>,
+    id: CanisterId,
+}
+
+impl Canister {
+    pub fn update_call<In, Out>(&self, sender: Principal, method: &str, args: In) -> Out
+    where
+        In: ArgumentEncoder + Send,
+        Out: CandidType + DeserializeOwned,
+    {
+        let message_id = self
+            .env
+            .submit_call(
+                self.id,
+                sender,
+                method,
+                encode_args(args).unwrap_or_else(|e| {
+                    panic!("Failed to encode arguments for method {method}: {e}")
+                }),
+            )
+            .unwrap_or_else(|e| panic!("Failed to call method {method}: {e}"));
+        let response_bytes = self
+            .env
+            .await_call_no_ticks(message_id)
+            .unwrap_or_else(|e| panic!("Failed to await call for method {method}: {e}"));
+        let (res,) = decode_args(&response_bytes).unwrap_or_else(|e| {
+            panic!("Failed to decode canister response for method {method}: {e}")
+        });
+        res
+    }
+}
