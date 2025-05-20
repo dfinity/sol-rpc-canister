1699bb871685a69636efd8a046f579ab3ae3fd9c
Louis Pahlavi <louis.pahlavi@gmail.com>
2025-05-20 13:53:08 +0200

    refactor: Serialize `VecWithMaxLen` properly (#109)
    (XC-291) Serialize the new `VecWithMaxLen` type to `Vec` (i.e. add
    `#[serde(into = "Vec<T>")]` annotation to type) and add some unit tests
    to ensure encoding/decoding round trips with Candid are successful when
    expected or fail when the underlying data is too long.

--- libs/types/src/lib.rs
+++ libs/types/src/lib.rs
@@ -21,7 +21,7 @@ pub use rpc_client::{
     RegexSubstitution, RoundingError, RpcAccess, RpcAuth, RpcConfig, RpcEndpoint, RpcError,
     RpcResult, RpcSource, RpcSources, SolanaCluster, SupportedRpcProvider, SupportedRpcProviderId,
 };
-use serde::Serialize;
+use serde::{Serialize, Serializer};
 pub use solana::{
     account::{AccountData, AccountEncoding, AccountInfo, ParsedAccount},
     request::{
@@ -44,7 +44,7 @@ pub use solana::{
 };
 
 /// A vector with a maximum capacity.
-#[derive(Debug, Clone, Deserialize, Serialize, CandidType, PartialEq, Default, Into)]
+#[derive(Debug, Clone, Deserialize, CandidType, PartialEq, Default, Into)]
 #[serde(try_from = "Vec<T>")]
 pub struct VecWithMaxLen<T, const CAPACITY: usize>(Vec<T>);
 
@@ -59,5 +59,14 @@ impl<T, const CAPACITY: usize> TryFrom<Vec<T>> for VecWithMaxLen<T, CAPACITY> {
             )));
         }
         Ok(Self(value))
+    }
+}
+
+impl<T: Serialize, const CAPACITY: usize> Serialize for VecWithMaxLen<T, CAPACITY> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.0.serialize(serializer)
     }
 }
--- libs/types/src/tests.rs
+++ libs/types/src/tests.rs
@@ -1,48 +1,86 @@
 use crate::VecWithMaxLen;
-use candid::{Decode, Encode};
+use candid::{CandidType, Decode, Encode};
 use proptest::{
-    arbitrary::any,
-    prelude::{prop, Strategy},
-    proptest,
+    arbitrary::Arbitrary,
+    prelude::{any, prop, Strategy, TestCaseError},
+    prop_assert, prop_assert_eq, proptest,
 };
-use serde::Deserialize;
-use serde_json::json;
+use serde::de::DeserializeOwned;
 
-proptest! {
-    #[test]
-    fn should_encode_decode (values in arb_vec_with_capacity()) {
-        let encoded = Encode!(&values).unwrap();
-        let decoded = Decode!(&encoded, VecWithMaxLen::<String, 100>).unwrap();
+mod vec_with_max_len_tests {
+    use super::*;
 
-        assert_eq!(decoded, values);
+    proptest! {
+        #[test]
+        fn should_encode_decode (
+            string_vec in arb_vec_with_max_size(50),
+            int_vec in arb_vec_with_max_size(75),
+            bytes_vec in arb_vec_with_max_size(100))
+        {
+            encode_decode_roundtrip::<String, 50>(string_vec)?;
+            encode_decode_roundtrip::<i32, 75>(int_vec)?;
+            encode_decode_roundtrip::<Vec<u8>, 100>(bytes_vec)?;
+        }
+
+        #[test]
+        fn should_fail_to_decode_when_too_long(
+            string_vec in arb_vec_with_size_greater_than(50),
+            int_vec in arb_vec_with_size_greater_than(75),
+            bytes_vec in arb_vec_with_size_greater_than(100))
+        {
+            expect_decoding_error::<String, 50>(string_vec)?;
+            expect_decoding_error::<i32, 75>(int_vec)?;
+            expect_decoding_error::<Vec<u8>, 100>(bytes_vec)?;
+        }
     }
 
-    #[test]
-    fn should_deserialize(values in prop::collection::vec(any::<String>(), 0..=100)) {
-        let serialized = json!(values);
-
-        let result = VecWithMaxLen::<String, 100>::deserialize(&serialized);
-
-        assert!(result.is_ok());
-        assert_eq!(Vec::from(result.unwrap()), values);
-    }
-
-    #[test]
-    fn should_not_deserialize(values in prop::collection::vec(any::<String>(), 101..1000)) {
-        let serialized = json!(values);
-
-        let result = VecWithMaxLen::<String, 100>::deserialize(&serialized);
-
-        assert!(result.is_err());
-        assert_eq!(
-            result.err().unwrap().to_string(),
-            format!("Validation error: Expected at most 100 items, but got {}", values.len())
+    fn encode_decode_roundtrip<T, const CAPACITY: usize>(value: Vec<T>) -> Result<(), TestCaseError>
+    where
+        T: Clone + CandidType + std::fmt::Debug + PartialEq + DeserializeOwned,
+    {
+        let wrapped_value: VecWithMaxLen<T, CAPACITY> = TryFrom::try_from(value.clone())?;
+        let encoded_value = Encode!(&value)?;
+        let encoded_wrapped_value = Encode!(&wrapped_value)?;
+        prop_assert_eq!(
+            &encoded_value,
+            &encoded_wrapped_value,
+            "Encoded value differ for {:?}",
+            value
         );
-    }
-}
 
-fn arb_vec_with_capacity<const CAPACITY: usize>(
-) -> impl Strategy<Value = VecWithMaxLen<String, CAPACITY>> {
-    prop::collection::vec(any::<String>(), 0..=CAPACITY)
-        .prop_map(|values| values.try_into().unwrap())
+        let decoded_value = Decode!(&encoded_value, VecWithMaxLen<T, CAPACITY>)?;
+        prop_assert_eq!(
+            &decoded_value,
+            &wrapped_value,
+            "Decoded value differ for {:?}",
+            value
+        );
+        Ok(())
+    }
+
+    fn expect_decoding_error<T, const CAPACITY: usize>(
+        too_long: Vec<T>,
+    ) -> Result<(), TestCaseError>
+    where
+        T: Clone + CandidType + std::fmt::Debug + PartialEq + DeserializeOwned,
+    {
+        let result = Decode!(&Encode!(&too_long)?, VecWithMaxLen<T, CAPACITY>);
+        prop_assert!(
+            result.is_err(),
+            "Expected error decoding {:?}, got: {:?}",
+            too_long,
+            result
+        );
+        Ok(())
+    }
+
+    fn arb_vec_with_max_size<T: Arbitrary>(max_size: usize) -> impl Strategy<Value = Vec<T>> {
+        prop::collection::vec(any::<T>(), 0..=max_size)
+    }
+
+    fn arb_vec_with_size_greater_than<T: Arbitrary>(
+        min_size: usize,
+    ) -> impl Strategy<Value = Vec<T>> {
+        prop::collection::vec(any::<T>(), min_size + 1..=min_size + 100)
+    }
 }
