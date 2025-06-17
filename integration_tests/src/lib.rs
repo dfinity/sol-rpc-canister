350736d9cc479cbff368af4c0f0e00005f1487e7
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-06-17 08:30:58 +0200

    XC-296: Use crates.io version of ic-metrics-assert

--- integration_tests/src/lib.rs
+++ integration_tests/src/lib.rs
@@ -5,7 +5,7 @@ use canlog::{Log, LogEntry};
 use ic_cdk::api::call::RejectionCode;
 use ic_http_types::{HttpRequest, HttpResponse};
 use ic_management_canister_types::{CanisterId, CanisterSettings};
-use ic_metrics_assert::{AsyncCanisterHttpQuery, MetricsAssert};
+use ic_metrics_assert::{MetricsAssert, PocketIcAsyncHttpQuery};
 use num_traits::ToPrimitive;
 use pocket_ic::{
     common::rest::{
@@ -230,17 +230,13 @@ impl Setup {
     }
 }
 
-#[async_trait]
-impl AsyncCanisterHttpQuery<RejectResponse> for Setup {
-    async fn http_query(&self, request: Vec<u8>) -> Result<Vec<u8>, RejectResponse> {
-        self.env
-            .query_call(
-                self.sol_rpc_canister_id,
-                Principal::anonymous(),
-                "http_request",
-                request,
-            )
-            .await
+impl PocketIcAsyncHttpQuery for Setup {
+    fn get_pocket_ic(&self) -> &PocketIc {
+        &self.env
+    }
+
+    fn get_canister_id(&self) -> CanisterId {
+        self.sol_rpc_canister_id
     }
 }
 
